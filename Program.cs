98a1903b6f947ee6ee0662719e2647b5433d0715
unknown <rajputnikita5666@gmail.com>
2021-09-03 13:46:53 +0530

    feat:Solving by using case statement

--- Program.cs
+++ Program.cs
@@ -4,34 +4,34 @@ namespace EmployeeWage
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
         static void Main(string[] args)
         {
-
-            PartTimeEmpWage();
-        }
-        public static void PartTimeEmpWage()
-        {
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else if (empCheck == IS_FULL_TIME)
-                empHrs = 8;
-            //else
-            //empHrs = 0;
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
-            Console.ReadKey();
+            Console.WriteLine("Emp Wage:" + empWage);
         }
-
     }
-
 }
 
