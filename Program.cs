6565ad662fa11012c63203ec717f6d542fd71a90
unknown <rajputnikita5666@gmail.com>
2021-09-03 13:39:19 +0530

    feat:Calculating PartTime Emp Wage

--- Program.cs
+++ Program.cs
@@ -14,16 +14,16 @@ namespace EmployeeWage
             //Constants
             int IS_PART_TIME = 1;
             int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+            int EMP_RATE_PER_HOUR = 8;
             //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else if (empCheck == IS_FULL_TIME)
                 empHrs = 8;
+            else if (empCheck == IS_FULL_TIME)
+                empHrs = 12;
             //else
             //empHrs = 0;
             empWage = empHrs * EMP_RATE_PER_HOUR;
@@ -32,6 +32,5 @@ namespace EmployeeWage
         }
 
     }
-
 }
 
